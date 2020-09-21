aaa3d2f8e28dfc4fd67b5456532676901f4ab6c3
karthiknalaparaju9525 <46438510+karthiknalaparaju9525@users.noreply.github.com>
2020-09-21 02:59:51 -0500

    Add files via upload

---
+++ ADDINGNUMBERS.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AJ
+ */
+public class ADDINGNUMBERS {
+  public static void main(String[ ] args ) {
+        Scanner sc= new Scanner(System.in);
+        int a;
+        int sum=0;
+        System.out.println("Enter the Number:");
+        a=sc.nextInt();
+        for(int i=0;i<a+1;i++){
+            
+            sum=sum+i;
+        }
+      
+      
+       System.out.println("ADDING THE GIVEN NUMBERS OF THE VALUE:"+sum);
+            
+      
+             
+        }    
+}
